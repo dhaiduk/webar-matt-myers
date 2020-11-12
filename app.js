6efd800fc1fde62b55f87e6d37ceaea7afe9f203
Dzmitry Haiduk <dementeyyy@gmail.com>
2020-11-12 14:59:34 +0300

    add marker events

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ AFRAME.registerComponent('hider-material', {
   },
 })
 
-
 AFRAME.registerComponent('next-button1', {
   init: function () {
     const nextButton1 = document.getElementById('nextbutton1')
@@ -67,7 +66,6 @@ AFRAME.registerComponent('next-button1', {
   }
 })
 
-
 AFRAME.registerComponent('next-button2', {
   init: function () {
     const nextButton2 = document.getElementById('nextbutton2')
@@ -75,7 +73,7 @@ AFRAME.registerComponent('next-button2', {
     const nextButton2Click = (EO) => {
       if (aMarker.getAttribute('rotation-reader81')) {
         aMarker.removeAttribute('rotation-reader81');
-        aMarker.setAttribute('rotation-reader121','');
+        aMarker.setAttribute('rotation-reader121', '');
       }
     };
     nextButton2.onclick = nextButton2Click;
